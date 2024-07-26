f4b89b9f31e6305bbf164b37e57d17a5b88750b5
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-07-26 20:22:13 +0530

    Add Some Tests

--- src/test/java/com/passcodes/app/AppTest.java
+++ src/test/java/com/passcodes/app/AppTest.java
@@ -5,7 +5,16 @@ import static org.junit.Assert.*;
 
 public class AppTest {
     @Test
-    public void testApp() {
+    public void testHelpArgument() {
+        App app = new App();
+        app.main(new String[]{"--help"});
+        assertTrue(true);
+    }
+    
+    @Test
+    public void testVersionArgument() {
+        App app = new App();
+        app.main(new String[]{"--version"});
         assertTrue(true);
     }
 }
